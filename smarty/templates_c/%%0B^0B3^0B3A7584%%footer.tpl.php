6f95c9dbc0c37e8557b88b27a2926f0aaa046e6b
root <root@Server.net6a.local>
2017-05-24 09:44:56 +0200

    Nettoyage du projet

--- smarty/templates_c/%%0B^0B3^0B3A7584%%footer.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.29, created on 2017-04-28 12:57:46
-         compiled from mobile/footer.tpl */ ?>
-  </body>
-</html>
--- smarty/templates_c/%%E4^E4C^E4C1CEEF%%wall_footer.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.29, created on 2017-04-28 12:39:43
-         compiled from wall_footer.tpl */ ?>
-  </body>
-</html>
